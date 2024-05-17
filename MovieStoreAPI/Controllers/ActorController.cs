0c4df4fc34e1d085005fa281eb36f209ee5d053a
Domagoj Licitar <licitar.domagoj@gmail.com>
2024-05-17 12:59:04 +0200

    Added two controllers with two Get methods

---
+++ MovieStoreAPI/Controllers/ActorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorController : ControllerBase
+    {
+        [HttpGet("pregledSvihFilmova")]
+        public IActionResult GetAllMovies()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ MovieStoreAPI/Controllers/DirectorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DirectorController : ControllerBase
+    {
+        [HttpGet("pregledVlastitihPodataka")]
+        public IActionResult GetMyData()
+        {
+            return Ok();
+        }
+    }
+}
