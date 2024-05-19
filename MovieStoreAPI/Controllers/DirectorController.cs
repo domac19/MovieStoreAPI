020e7459655e9e6ac3dadb09c71328e8204bf562
Domagoj Licitar <licitar.domagoj@gmail.com>
2024-05-19 15:15:03 +0200

    Fixed one get method and added new create update and delete methods

--- MovieStoreAPI/Controllers/DirectorController.cs
+++ MovieStoreAPI/Controllers/DirectorController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MovieStoreAPI.Context;
+using MovieStoreAPI.Models;
 
 namespace MovieStoreAPI.Controllers
 {
@@ -13,17 +14,69 @@ namespace MovieStoreAPI.Controllers
         {
             _appDbContext = appContext;
         }
+
         [Authorize]
         [HttpGet("pregledSvihFilmova")]
-        public IActionResult GetAllMovies()
+        public ActionResult<List<Movie>> GetAllMovies()
         {
-            return Ok(_appDbContext.Movie.FirstOrDefault());
+            return Ok(_appDbContext.Movie.ToList());
         }
 
         [HttpGet("pregledPojedinogFilma")]
         public IActionResult GetMoviesById(int id)
         {
             return Ok(_appDbContext.Movie.Where(x => x.Id == id).SingleOrDefault());
+        }
+
+        [HttpPost("kreiranjeFilma")]
+        public IActionResult CreateMovie([FromBody] Movie movie)
+        {
+            if (movie == null)
+            {
+                return BadRequest();
+            }
+
+            _appDbContext.Movie.Add(movie);
+            _appDbContext.SaveChanges();
+
+            return Ok(movie);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMovie([FromBody] Movie movie)
+        {
+            if (movie?.Id == null)            
+                return NotFound();
+            
+            var getById = _appDbContext.Movie.Find(movie.Id);
+
+            if (movie == null)
+                return NotFound();
+            
+            getById!.Title = movie.Title;
+            getById.StartOfFilming = movie.StartOfFilming;
+            getById.EndOfFilming = movie.EndOfFilming;
+            getById.Description = movie.Description;
+            getById.Budget = movie.Budget;
+            getById.Genre = movie.Genre;
+
+            _appDbContext.SaveChanges();
+            
+            return Ok(movie);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteMovieById(int id)
+        {
+            var getId = _appDbContext.Movie.Where(x => x.Id == id).FirstOrDefault();
+
+            if (getId == null)
+                return NotFound();
+
+            _appDbContext.Movie.Remove(getId);
+            _appDbContext.SaveChanges();
+
+            return Ok();
         }
     }
 }
