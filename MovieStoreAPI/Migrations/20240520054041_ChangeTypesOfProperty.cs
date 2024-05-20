546c068acb586ba39e672cd1b50fb0538f7baaa1
Domagoj Licitar <licitar.domagoj@gmail.com>
2024-05-20 07:41:36 +0200

    Added new migration for type of property

---
+++ MovieStoreAPI/Migrations/20240520054041_ChangeTypesOfProperty.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieStoreAPI.Migrations
+{
+    public partial class ChangeTypesOfProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Duration",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Duration",
+                table: "Movie",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- MovieStoreAPI/Models/Movie.cs
+++ MovieStoreAPI/Models/Movie.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal Budget { get; set; }
-        public double Duration { get; set; }
+        public string Duration { get; set; }
         public DateTime StartOfFilming { get; set; }
         public DateTime EndOfFilming { get; set; }
         public string Genre { get; set; }
